7069f5fad06df926a9e12de5c9cda4b3650acc4c
Atif Chowdhury <iftakhar.awal@gmail.com>
2023-08-10 12:47:42 +0600

    Icon changes

--- src/LoginFrame.java
+++ src/LoginFrame.java
@@ -7,88 +7,111 @@ import java.awt.event.ActionListener;
 public class LoginFrame extends JFrame implements ActionListener {
     private final JPanel loginPanel;
     private final JPanel imagePanel;
+    private final JLabel introLabel;
+
     private final JLabel mailLabel;
     private final JLabel passLabel;
     private final JTextField mailField;
     private final JPasswordField passField;
     private final JToggleButton showHideButton;
-    private final JLabel introLabel;
+    private JCheckBox rememberBox;
+    private JButton forgotButton;
     private final JPanel buttonPanel;
     private final JButton loginButton;
     private final Color backgroundColor;
     private final ImageIcon showIcon;
     private final ImageIcon hideIcon;
+    private Font introFont;
+    private Font normalFont;
+    private Font smallFont;
+    private Font fieldFont;
 
     public LoginFrame() {
         this.setTitle("Login Window");
-        this.setSize(900, 500);
+        this.setSize(1260, 700);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setLayout(new BorderLayout());
 
         backgroundColor = new Color(192, 192, 192);
+        introFont = new Font("Century Gothic", Font.BOLD, 25);
+        normalFont = new Font("Inter", Font.PLAIN, 16);
+        smallFont = new Font("Inter", Font.PLAIN, 14);
+        fieldFont = new Font("Inter", Font.PLAIN, 18);
 
         loginPanel = new JPanel();
-        loginPanel.setPreferredSize(new Dimension(400, 500));
+        loginPanel.setPreferredSize(new Dimension(560, 700));
         loginPanel.setBackground(backgroundColor);
 
         introLabel = new JLabel("Hello, World!");
-        introLabel.setBorder(new EmptyBorder(50, 0, 40, 0));
-        introLabel.setFont(new Font("Century Gothic", Font.BOLD, 18));
+        introLabel.setBorder(new EmptyBorder(70, 0, 56, 0));
+        introLabel.setFont(introFont);
         loginPanel.add(introLabel);
 
         mailLabel = new JLabel("Email address");
-        mailLabel.setPreferredSize(new Dimension(300, 20));
-        // mailLabel.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+        mailLabel.setFont(normalFont);
+        mailLabel.setPreferredSize(new Dimension(420, 28));
         loginPanel.add(mailLabel);
 
         mailField = new JTextField();
-        mailField.setPreferredSize(new Dimension(300, 40));
+        mailField.setPreferredSize(new Dimension(420, 56));
+        mailField.setFont(fieldFont);
         mailField.setToolTipText("Enter your email address");
-        // mailField.setBorder(null);
-        // mailField.setBackground(backgroundColor);
+        mailField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        mailField.setBackground(backgroundColor);
         loginPanel.add(mailField);
 
         passLabel = new JLabel("Password");
-        passLabel.setPreferredSize(new Dimension(300, 20));
-        // passLabel.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+        passLabel.setFont(normalFont);
+        passLabel.setPreferredSize(new Dimension(420, 28));
         loginPanel.add(passLabel);
 
         passField = new JPasswordField();
-        passField.setPreferredSize(new Dimension(265, 40));
+        passField.setPreferredSize(new Dimension(390, 56));
+        passField.setFont(fieldFont);
         passField.setToolTipText("Enter your password");
-        // passField.setBorder(null);
-        // passField.setBackground(backgroundColor);
+        passField.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.BLACK));
+        passField.setBackground(backgroundColor);
         loginPanel.add(passField);
 
         showIcon = new ImageIcon("src/images/show.png");
         hideIcon = new ImageIcon("src/images/hide.png");
 
         showHideButton = new JToggleButton(showIcon);
-        showHideButton.setPreferredSize(new Dimension(30, 40)); // Set button dimensions
+        showHideButton.setPreferredSize(new Dimension(25, 56));
         showHideButton.setFocusable(false);
         showHideButton.setBackground(backgroundColor);
         UIManager.put("ToggleButton.select", backgroundColor);
         SwingUtilities.updateComponentTreeUI(showHideButton);
-        showHideButton.setBorder(null);
+        showHideButton.setBorderPainted(false);
         showHideButton.addActionListener(this);
         loginPanel.add(showHideButton);
 
+        rememberBox = new JCheckBox("Remember me");
+        rememberBox.setPreferredSize(new Dimension(420, 40));
+        rememberBox.setFocusable(false);
+        rememberBox.setIcon(new ImageIcon("src/images/checkbox_blank.png"));
+        rememberBox.setSelectedIcon(new ImageIcon("src/images/checkbox_selected.png"));
+        rememberBox.setFont(smallFont);
+        rememberBox.setBackground(backgroundColor);
+        loginPanel.add(rememberBox);
+
         buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(1, 2));
-        buttonPanel.setBorder(new EmptyBorder(20, 0, 20, 0));
-        buttonPanel.setPreferredSize(new Dimension(300, 80));
+        buttonPanel.setBorder(new EmptyBorder(28, 0, 28, 0));
+        buttonPanel.setPreferredSize(new Dimension(420, 110));
         buttonPanel.setBackground(backgroundColor);
 
         loginButton = new JButton("Log in");
+        loginButton.setFont(normalFont);
         loginButton.setFocusable(false);
         loginButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
         buttonPanel.add(loginButton);
         loginPanel.add(buttonPanel);
 
         imagePanel = new JPanel();
-        imagePanel.setPreferredSize(new Dimension(500, 500));
+        imagePanel.setPreferredSize(new Dimension(700, 700));
 
         this.add(loginPanel, BorderLayout.WEST);
         this.add(imagePanel, BorderLayout.EAST);
@@ -104,7 +127,6 @@ public class LoginFrame extends JFrame implements ActionListener {
                 passField.setEchoChar((char) UIManager.get("PasswordField.echoChar"));
                 showHideButton.setIcon(showIcon);
             }
-            showHideButton.repaint();
         }
     }
 }
